a579e7bdeaaca448c8b4702daa9648d6cc6a8caf
Andy McKay <andy@clearwind.ca>
2023-03-29 15:28:09 -0700

    add in whitenoise

--- catalog/settings.py
+++ catalog/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 OAUTH_LOGIN_PROVIDERS = {
     "github": {
