842857bf49906f514f73316832e8ccf9dfc69805
Andy McKay <andy@clearwind.ca>
2023-05-17 12:35:13 -0700

    fix tests

--- services/tests.py
+++ services/tests.py
@@ -27,13 +27,16 @@ def create_source():
 
 def create_service(source):
     """Create a service."""
+    name = fake.user_name()
+    description = fake.text()
     return models.Service.objects.create(
-        name=fake.user_name(),
-        description=fake.text(),
+        name=name,
+        description=description,
         type="application",
         source=source,
         meta={"foo": "bar"},
         events=["deployments", "releases"],
+        raw_data={"name": name, "description": description, "type": "application", "meta": {"foo": "bar"}, "events": ["deployments", "releases"]},
     )
 
 
